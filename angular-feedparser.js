7de8c7bccbcfeb5f6b47324e2b01fa8ac177db36
Josepo <jlpolo@gmail.com>
2015-10-02 14:33:36 +0200

    Get entries info for ATOM files

--- angular-feedparser.js
+++ angular-feedparser.js
@@ -8,13 +8,35 @@ angular.module('angular-feedparser').service('feedparser', function(x2js) {
    var me = this;
    
    me.parse = function(xmldata) {
-      var jsondata = x2js.xml_str2json(xmldata);
+      var feed = x2js.xml_str2json(xmldata).feed,
+         entries = [];
+      
+      feed.entry.forEach(function(entry) {
+         var linkfound = false,
+            i = 0,
+            link;
+            
+         while (!linkfound && (i < entry.link.length)) {
+            if (entry.link[i]._rel == 'alternate') {
+               linkfound = true;
+               link = entry.link[i]._href;
+            } else {
+               i++;
+            }
+         }
+
+         entries.push({
+            title: entry.title.__cdata,
+            content: entry.content.__cdata,
+            link: link
+         });
+      });
       
       return {
-         title: jsondata.feed.title.__text,
-         subtitle: jsondata.feed.subtitle.__text,
-         updated: new Date(jsondata.feed.updated),
-         entries: []
-      }
+         title: feed.title.__text,
+         subtitle: feed.subtitle.__text,
+         updated: new Date(feed.updated),
+         entries: entries
+      };
    };
 });
--- test/feedparser.spec.js
+++ test/feedparser.spec.js
@@ -27,6 +27,15 @@ describe('[angular-feedparser] ', function () {
       expect(info.subtitle).toBeUndefined();
       expect(info.updated).toEqual(new Date('2015-09-30T05:57:45Z'));
     });
+    
+    it('should get entries info', function() {
+      var entries = feedparser.parse(atom).entries;
+      
+      expect(entries.length).toBe(10);
+      expect(entries[0].title).toBe('Show some respect for the brave.');
+      expect(entries[0].link).toBe('http://www.alexandrafranzen.com/2015/09/29/respect/');
+      expect(entries[0].content).toBeDefined();
+    });
   });
 
 });
